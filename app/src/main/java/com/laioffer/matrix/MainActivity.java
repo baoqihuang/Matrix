a1c226cdaf4cb32074a80915adecaa2942b03927
Michael Huang <hbqljy@gmail.com>
2019-09-04 08:21:06 -0700

    android test

---
+++ app/src/main/java/com/laioffer/matrix/MainActivity.java
@@ -0,0 +1,74 @@
+package com.laioffer.matrix;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Get ListView object from xml.
+        ListView eventListView = (ListView) findViewById(R.id.event_list);
+
+        // Initialize an adapter.
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this,
+                R.layout.event_item,
+                R.id.event_name,
+                getEventNames());
+
+        // Assign adapter to ListView.
+        eventListView.setAdapter(adapter);
+    }
+
+    /**
+     * A dummy function to get fake event names.
+     */
+    private String[] getEventNames() {
+        String[] names = {
+                "Event1", "Event2", "Event3",
+                "Event4", "Event5", "Event6",
+                "Event7", "Event8", "Event9",
+                "Event10", "Event11", "Event12"};
+        return names;
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.e("Life cycle test", "We are at onStart()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.e("Life cycle test", "We are at onResume()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.e("Life cycle test", "We are at onPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.e("Life cycle test", "We are at onStop()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e("Life cycle test", "We are at onDestroy()");
+    }
+
+}
